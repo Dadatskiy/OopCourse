c70c3fc74a163f2681b4f7cbc701015f36134c77
admin <dadatskiyvv@gmail.com>
2022-08-11 03:14:17 +0700

    Add Range*

--- Range/src/dada/oop/range/Range.java
+++ Range/src/dada/oop/range/Range.java
@@ -32,4 +32,59 @@ public class Range {
     public boolean isInside(double number) {
         return number >= from && number <= to;
     }
+
+    public Range getIntersection(double newFrom, double newTo) {
+        if (to > newFrom && newTo > from) {
+            if (from > newFrom && to > newTo) {
+                return new Range(from, newTo);
+            } else if (from < newFrom && to < newTo) {
+                return new Range(newFrom, to);
+            } else if (from > newFrom && to < newTo) {
+                return new Range(from, to);
+            } else if (from < newFrom && to > newTo) {
+                return new Range(newFrom, newTo);
+            }
+        }
+
+        return null;
+    }
+
+    public Range[] getUnion(double newFrom, double newTo) {
+        if (from == newFrom && to == newTo) {
+            return null;
+        }
+
+        if (to > newFrom && newTo > from) {
+            if (from > newFrom && to > newTo) {
+                return new Range[]{new Range(newFrom, to)};
+            } else if (from < newFrom && to < newTo) {
+                return new Range[]{new Range(from, newTo)};
+            } else if (from > newFrom && to < newTo) {
+                return new Range[]{new Range(newFrom, newTo)};
+            } else if (from < newFrom && to > newTo) {
+                return new Range[]{new Range(from, to)};
+            }
+        }
+
+        return from < newFrom ? new Range[]{new Range(from, to), new Range(newFrom, newTo)} :
+                new Range[]{new Range(newFrom, newTo), new Range(from, to)};
+    }
+
+    public Range[] getDifference(double newFrom, double newTo) {
+        if (to > newFrom && newTo > from) {
+            if (from > newFrom && to > newTo) {
+                return new Range[]{new Range(newTo, to)};
+            } else if (from < newFrom && to < newTo) {
+                return new Range[]{new Range(from, newFrom)};
+            } else if (from < newFrom && to > newTo) {
+                return new Range[]{new Range(from, newFrom), new Range(newTo, to)};
+            }
+        }
+
+        if ((from > newFrom && to < newTo) || (from < newFrom && to < newTo) || (from == newFrom && to == newTo)) {
+            return null;
+        }
+
+        return new Range[]{new Range(from, newFrom), new Range(newTo, to)};
+    }
 }
--- Range/src/dada/oop/range_main/Main.java
+++ Range/src/dada/oop/range_main/Main.java
@@ -16,8 +16,7 @@ public class Main {
 
         Range range = new Range(from, to);
 
-        System.out.println("Длина диапазона ["
-                + range.getFrom() + " ; " + range.getTo() + "] = " + range.getLength());
+        System.out.println("Длина диапазона (" + range.getFrom() + " ; " + range.getTo() + ") = " + range.getLength());
 
         System.out.print("Введите любое число, чтобы проверить, лежит ли оно в заданном вами диапазоне: ");
         double number = scanner.nextDouble();
@@ -31,23 +30,42 @@ public class Main {
         System.out.print("Задайте заново первое число диапазона: ");
         double newFrom = scanner.nextDouble();
 
-        range.setFrom(newFrom);
-
         System.out.print("Задайте заново последнее число диапазона: ");
         double newTo = scanner.nextDouble();
 
-        range.setTo(newTo);
+        Range intersectionRange = range.getIntersection(newFrom, newTo);
 
-        System.out.println("Длина нового диапазона чисел ["
-                + range.getFrom() + " ; " + range.getTo() + "] = " + range.getLength());
-
-        System.out.print("Введите любое число, чтобы проверить, лежит ли оно в заданном вами диапазоне: ");
-        number = scanner.nextDouble();
-
-        if (range.isInside(number)) {
-            System.out.println("Это число лежит в заданном диапазоне.");
+        if (intersectionRange == null) {
+            System.out.println("Заданные интервалы не пересекаются");
         } else {
-            System.out.println("Это число не лежит в заданном диапазоне.");
+            System.out.println("Интервал пересечения двух интервалов: "
+                    + "(" + intersectionRange.getFrom() + ", " + intersectionRange.getTo() + ")");
+        }
+
+        Range [] unionRange = range.getUnion(newFrom, newTo);
+
+        if (unionRange == null) {
+            System.out.println("Заданные интервалы невозможно объединить");
+        }
+        else if (unionRange.length == 1) {
+            System.out.println("Интервал объединения двух интервалов: " + "(" + unionRange[0].getFrom() + ", " + unionRange[0].getTo()+ ")");
+        } else {
+            System.out.println("Интервалы объединения двух интервалов: "
+                    + "(" + unionRange[0].getFrom() + ", " + unionRange[0].getTo() + "), ("
+                    + unionRange[1].getFrom() + ", " + unionRange[1].getTo() + ")");
+        }
+
+
+        Range [] differenceRange = range.getDifference(newFrom, newTo);
+
+        if (differenceRange == null) {
+            System.out.println("Нессимитричая разность данных интервалов отсутствует.");
+        } else if (differenceRange.length == 1) {
+            System.out.println("Интервал разности двух интервалов: " + "(" + differenceRange[0].getFrom() + ", " + differenceRange[0].getTo()+ ")");
+        } else if (differenceRange.length == 2) {
+            System.out.println("Интервалы разности двух интервалов: "
+                    + "(" + differenceRange[0].getFrom() + ", " + differenceRange[0].getTo() + "), ("
+                    + differenceRange[1].getFrom() + ", " + differenceRange[1].getTo() + ")");
         }
     }
 }
