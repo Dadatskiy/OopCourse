4d8663566dc9309387ea76601a41d4c9660c62f0
Range <dadatskiyvv@gmail.com>
2022-08-29 22:21:06 +0700

    Себе на доработку

--- Matrix/src/dada/oop/matrix/Matrix.java
+++ Matrix/src/dada/oop/matrix/Matrix.java
@@ -3,45 +3,69 @@ package dada.oop.matrix;
 import dada.oop.vector.Vector;
 
 public class Matrix {
-    private int n; // height
-    private int m; // width
-    private double[][] array;
+    private int m; //  rowsCount
+    private int n; // columnsCount
+    public double[][] array;
     private Vector[] vectors;
 
-    public Matrix(int n, int m) {
-        if (n < 1 || m < 1) {
-            throw new IllegalArgumentException("Матрица размерами " + n + " х " + m
-                    + " не может существовать. " + "Количество столбцов или строк в матрице не может быть меньше одного");
+    public Matrix(int m, int n) {
+        if (m < 1 || n < 1) {
+            throw new IllegalArgumentException("Матрица размерами " + m + " х " + n
+                    + " не может существовать. " + "Количество строк или столбцов в матрице не может быть меньше одного");
         }
 
-        this.n = n;
         this.m = m;
-        this.array = new double[n][m];
+        this.n = n;
+        this.array = new double[m][n];
         this.vectors = arrayToVectors();
     }
 
     public Matrix(Matrix matrix) {
-        this.n = matrix.n;
         this.m = matrix.m;
+        this.n = matrix.n;
         this.array = matrix.array;
         this.vectors = matrix.vectors;
     }
 
     public Matrix(double[][] array) {
-        this.n = array.length;
-        this.m = array[0].length;
+        this.m = array.length;
+        this.n = array[0].length;
         this.array = array;
         this.vectors = arrayToVectors();
     }
 
     public Matrix(Vector[] vectors) {
+        this.m = vectors.length;
+
+        int maxLength = 0;
+
+        for (int i = 0; i < m; i++) {
+            maxLength = Math.max(maxLength, vectors[i].getSize());
+        }
+
+        this.n = maxLength;
+
+        for (int i = 0; i < m; i++) {
+            vectors[i].toCompleteVector(n);
+        }
+
         this.vectors = vectors;
+        this.array = new double[m][n];
+        vectorsToArray();
+    }
+
+    private void vectorsToArray() {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                array[i][j] = vectors[i].getComponent(j);
+            }
+        }
     }
 
     private Vector[] arrayToVectors() {
-        Vector[] vectorsFromArray = new Vector[array.length];
+        Vector[] vectorsFromArray = new Vector[m];
 
-        for (int i = 0; i <= array.length - 1; i++) {
+        for (int i = 0; i < m; i++) {
             vectorsFromArray[i] = fromRawToVector(i);
         }
 
@@ -52,22 +76,72 @@ public class Matrix {
         return new Vector(array[rowNumber]);
     }
 
+    public int getRowsCount () {
+        return m;
+    }
+
+    public int getColumnsCount () {
+        return n;
+    }
+
+    public Vector getRowVector (int rowIndex) {
+        return vectors[rowIndex];
+    }
+
+    public void setRowVector (int rowNumber, Vector vector) {
+        vectors[rowNumber] = vector;
+        array[rowNumber] = vector.getComponents();
+    }
+
+    public Vector getColumnVector (int columnIndex) {
+        Vector vector = new Vector(m);
+
+        for (int i = 0; i < m; i++) {
+            vector.setComponent(i, array[i][columnIndex]);
+        }
+
+        return vector;
+    }
+
+    public Matrix transpose () {
+        Matrix matrix  = new Matrix(n,m);
+
+        for (int i = 0; i < n; i++) {
+            matrix.setRowVector(i, this.getColumnVector(i));
+            vectors[i] = matrix.getRowVector(i);
+        }
+
+        int temp = m;
+        m = n;
+        n = temp;
+
+        array = new double[m][n];
+        vectorsToArray();
+
+        return matrix;
+    }
+/*
     @Override
     public String toString() {
         StringBuilder matrixString = new StringBuilder("{ ");
-/*
-        for (int i = 0; i < array.length - 1; i++) {
-            matrixString.append((fromRawToVector(i))).append(", ");
-        }
 
-        matrixString.append(fromRawToVector(array.length - 1)).append("}");
-*/
-        for (int i = 0; i < array.length - 1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             matrixString.append((vectors[i])).append(", ");
         }
 
-        matrixString.append(vectors[array.length - 1]).append(" }");
+        matrixString.append(vectors[n - 1]).append(" }");
+        return String.valueOf(matrixString);
+    }
+*/
+    @Override
+    public String toString() {
+        StringBuilder matrixString = new StringBuilder();
 
+        for (int i = 0; i < m - 1; i++) {
+            matrixString.append((vectors[i])).append("\n");
+        }
+
+        matrixString.append(vectors[m - 1]);
         return String.valueOf(matrixString);
     }
 }
--- Matrix/src/dada/oop/matrix_main/Main.java
+++ Matrix/src/dada/oop/matrix_main/Main.java
@@ -1,26 +1,46 @@
 package dada.oop.matrix_main;
 
 import dada.oop.matrix.Matrix;
+import dada.oop.vector.Vector;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
-        double[][] array = new double[4][4];
+        double[][] array = new double[5][3];
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
+            for (int j = 0; j < array[0].length; j++) {
                 array[i][j] = 2 * i + 5 * j;
             }
         }
 
         Matrix matrix = new Matrix(array);
 
+        System.out.println(array.length);
+        System.out.println(matrix);
+        System.out.println();
+        System.out.println(matrix.transpose());
 
+/*
         int length = array.length;
         System.out.println(length);
         System.out.println(matrix);
 
+        double[] components1 = {15, 30, 45, 60};
+        double[] components2 = {30, 50};
+        double[] components3 = {0, 10, 20, 30, 40, 50};
 
 
+        Vector vector1 = new Vector(components1);
+        Vector vector2 = new Vector(components2);
+        Vector vector3 = new Vector(components3);
+
+        Vector[] vectors = {vector1, vector2, vector3};
+
+        Matrix matrix = new Matrix(vectors);
+        System.out.println(Arrays.toString(matrix.getMatrixSizes()));
+ */
 
     }
 }
