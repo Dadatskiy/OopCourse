1f450d89ee1cf4cb41d397482d60331b04eb1ee7
admin <dadatskiyvv@gmail.com>
2022-10-20 11:08:17 +0700

    Себе на доработку

--- List/src/dada/oop/list/SinglyLinkedList.java
+++ List/src/dada/oop/list/SinglyLinkedList.java
@@ -19,12 +19,11 @@ public class SinglyLinkedList<T> {
 
     @Override
     public String toString() {
-        int i;
         ListItem<T> p = head;
 
         StringBuilder stringBuilder = new StringBuilder("[");
 
-        for (i = 0; i < count - 1; i++) {
+        for (int i = 0; i < count - 1; i++) {
             stringBuilder.append(p.getData());
             stringBuilder.append(", ");
             p = p.getNext();
@@ -41,10 +40,9 @@ public class SinglyLinkedList<T> {
     }
 
     public T getData(int index) {
-        int i;
-        ListItem<T> p;
+        ListItem<T> p = head;
 
-        for (p = head, i = 0; i < index; p = p.getNext()) {
+        for (int i = 0; i < index; p = p.getNext()) {
             i++;
         }
 
@@ -52,10 +50,9 @@ public class SinglyLinkedList<T> {
     }
 
     public T setData(int index, T data) {
-        int i;
-        ListItem<T> p;
+        ListItem<T> p = head;
 
-        for (p = head, i = 0; i < index; p = p.getNext()) {
+        for (int i = 0; i < index; p = p.getNext()) {
             i++;
         }
 
@@ -66,11 +63,10 @@ public class SinglyLinkedList<T> {
         return originalData;
     }
 
-    public T deleteElement(int index) {
-        int i;
-        ListItem<T> p;
+    public T deleteItemByIndex(int index) {
+        ListItem<T> p = head;
 
-        for (p = head, i = 0; i < index - 1; p = p.getNext()) {
+        for (int i = 0; i < index - 1; p = p.getNext()) {
             i++;
         }
 
@@ -89,5 +85,67 @@ public class SinglyLinkedList<T> {
         head.setNext(originalHead);
     }
 
+    public void add(ListItem<T> listItem, int index) {
+        if (index == 0) {
+            addFirst(listItem);
+            return;
+        }
 
+        ListItem<T> p = head;
+
+        for (int i = 0; i < index - 1; i++) {
+            p = p.getNext();
+        }
+
+        count++;
+        ListItem<T> newListItem = new ListItem<>(listItem.getData(), listItem.getNext());
+        newListItem.setNext(p.getNext());
+        p.setNext(newListItem);
+    }
+
+    public boolean deleteItemByData(T data) {
+        ListItem<T> p = head;
+
+        for (int i = 0; i < count - 1; i++) {
+            if (p.getNext().getData() == data) {
+                if (i == count - 1) {
+                    p.setNext(p.getNext().getNext());
+                }
+                p.getNext().setNext(p.getNext().getNext().getNext());
+                count--;
+
+                return true;
+            }
+
+            System.out.println(getData(i));
+            p = p.getNext();
+        }
+
+        return false;
+    }
+
+    public T deleteFirstItem() {
+        T firstData = head.getData();
+        head = head.getNext();
+        count--;
+        return firstData;
+    }
+
+    public void revert() {
+        ListItem<T> previous = null;
+        ListItem<T> p = head.getNext();
+        head.setNext(null);
+
+        while (p != null) {
+            ListItem<T> next = p.getNext();
+            p.setNext(head);
+            head = p;
+            p = next;
+        }
+    }
+
+    /*public SinglyLinkedList<T> copy() {
+
+        SinglyLinkedList listCopy = new SinglyLinkedList();
+    }*/
 }
--- List/src/dada/oop/list_main/Main.java
+++ List/src/dada/oop/list_main/Main.java
@@ -7,16 +7,28 @@ public class Main {
     public static void main(String[] args) {
         int listSize = 0;
 
-        ListItem<Integer> fourth = new ListItem<>(-10);
-        ListItem<Integer> third = new ListItem<>(42, fourth);
-        ListItem<Integer> second = new ListItem<>(12, third);
-        ListItem<Integer> first = new ListItem<Integer>(23, second);
+        ListItem<Integer> sixth = new ListItem<>(60);
+        ListItem<Integer> fifth = new ListItem<>(50, sixth);
+        ListItem<Integer> fourth = new ListItem<>(40, fifth);
+        ListItem<Integer> third = new ListItem<>(30, fourth);
+        ListItem<Integer> second = new ListItem<>(20, third);
+        ListItem<Integer> first = new ListItem<>(10, second);
 
-        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>(first);
-        //System.out.println(list.getFirstData());
-        //System.out.println(list.getCount());
+        ListItem<Integer> sixthCopy = new ListItem<>(third);
+
+        sixthCopy.setData(555);
+
+        System.out.println(third);
+
+
+
+
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>(first);
         System.out.println(list);
-        list.addFirst(third);
+        list.revert();
+        //System.out.println(list.getFirstData());
+        //System.out.println(list.deleteFirstItem());
+        //list.add(fourth,0);
         System.out.println(list);
     }
 }
